8854ebf7520cc9425d13005e5ca0524b4b454911
Tristan Kretzer <git@kretzer.dev>
2025-08-11 19:23:05 +0000

    fix(podman): set subgid for user in `setup_subuid_and_subgid.sh`

--- src/podman/scripts/setup_subuid_and_subgid.sh
+++ src/podman/scripts/setup_subuid_and_subgid.sh
@@ -20,5 +20,5 @@ if [ "$(id -u "$USER_NAME")" != 0 ]; then
   USER_ID="$(id -u "$USER_NAME")"
   GROUP_ID="$(id -g "$USER_NAME")"
   
-  usermod --add-subuids 1-$((USER_ID - 1)) --add-subuids $((USER_ID + 1))-65535 --add-subuids 1-$((GROUP_ID - 1)) --add-subuids $((GROUP_ID + 1))-65535 "${USER_NAME}"
+  usermod --add-subuids 1-$((USER_ID - 1)) --add-subuids $((USER_ID + 1))-65535 --add-subgids 1-$((GROUP_ID - 1)) --add-subgids $((GROUP_ID + 1))-65535 "${USER_NAME}"
 fi
